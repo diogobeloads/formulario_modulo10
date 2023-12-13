cda1b2f69411414a9e22acbd6a3b61e65756cdae
Diogo Belo <diogobelodss22@gmail.com>
2023-12-12 21:00:17 -0300

    Correção do arquivo main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 $("#telefone").mask("(00) 00000-0000");
 $("#cpf").mask("000.000.000-00");
 $("#cep").mask("00000-000");
-$(".menu-hamburguer").click(function () {
-  $("nav").slideToggle();
-});
 $("form").validate({
   rules: {
     nome: {
@@ -21,8 +18,23 @@ $("form").validate({
     cep: {
       required: true,
     },
-    endereco: {
-      required: true,
-    },
   },
 });
+
+function validarFormulario() {
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  let cpf = document.getElementById("cpf").value;
+  let cep = document.getElementById("cep").value;
+  let telefone = document.getElementById("telefone").value;
+
+  // Para verificar se os campos nome e email estão preenchidos corretamente
+  if (nome === "" || email === "") {
+    alert("Por favor, preencha todos os campos.");
+  } else {
+    // Se todos os campos estiverem preenchidos, exibir um alerta de confirmação
+    alert(
+      "Formulário enviado com sucesso!\nNome: " + nome + "\nE-mail: " + email
+    );
+  }
+}
